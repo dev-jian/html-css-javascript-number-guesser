85d37dc9231f58f0da95a26e1c5708ff4caad0ef
dev_jian <devjian1123@gmail.com>
2021-06-04 12:47:09 +0900

    add logic when players lost, add gameOver function

--- app.js
+++ app.js
@@ -18,27 +18,52 @@ uiMaxNum.textContent = max;
 
 // Listener for guess btn
 uiGuessBtn.addEventListener("click", function (e){
+  setMessage("", "");
+  
   let guess = parseInt(uiGuessInput.value);
 
   // Validate
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, "red");
   } else {
-    // check if won
+    // check if game won
     if (guess === winningNum) {
-      // disable input
-      uiGuessInput.disabled = true;
-
-      // change border color
-      uiGuessInput.style.borderColor = "green";
-
-      // set msg
-      setMessage(`${winningNum} is correct!, YOU WIN!`, "green");
+      // Game over - won
+      gameOver(true, `Please enter a number between ${min} and ${max}`);
     } else {
-  
+      // Wrong number
+      guessesLeft -= 1;
+
+      if (guessesLeft === 0) {
+        // Game over - lost
+        gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+      } else {
+        // Game continue - answer wrong
+        // change border color
+        uiGuessInput.style.borderColor = "red";
+        
+        uiGuessInput.value = "";
+
+        // tell user its the wrong number
+        setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, "red");
+      }
     }
   }
 });
+
+// Game over
+function gameOver(isWon, msg) {
+  const color = isWon ? "green" : "red";
+  
+  // disable input
+  uiGuessInput.disabled = true;
+
+  // change border color
+  uiGuessInput.style.borderColor = color;
+
+  // set msg
+  setMessage(msg, color);
+}
 
 // Set Message
 function setMessage(msg, color) {
